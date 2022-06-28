070fdb04716f26dbcec6db985b4eac06d3ad6d19
leqnminh <leqnminh22@gmail.com>
2022-06-28 16:27:04 +0300

    Initial commit

---
+++ app/src/main/java/com/mle/weatherappnew/view/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.mle.weatherappnew.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mle.weatherappnew.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/com/mle/weatherappnew/viewmodel/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.mle.weatherappnew.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
